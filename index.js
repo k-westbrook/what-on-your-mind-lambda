53cad75905fde30d575b59b193ae389129ecd2c0
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-02 21:56:32 -0700

    updated comment add

---
+++ index.js
@@ -0,0 +1,55 @@
+const uuidV4 = require('uuid/v4');
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-west-1" });
+
+exports.handler = (event, context, callback) => {
+  const params = {
+    Item: {
+      comment_id: uuidV4(),
+      category: event.category,
+      comment: event.comment,
+      date: event.date
+    },
+    TableName: "Comments"
+  };
+
+  docClient.put(params, function (err, data) {
+
+    if (err) {
+
+      data = {
+        statusCode: 403,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: {
+          message: JSON.stringify('ERROR, comment not added.')
+        }
+      };
+      callback(null, data);
+
+    } else {
+
+      data = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: {
+
+          message: JSON.stringify('Successful creation in table.'),
+          user: params.Item
+        }
+
+      };
+      callback(null, data);
+
+    }
+  })
+
+
+
+
+};
